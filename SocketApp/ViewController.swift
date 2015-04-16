c7949dfa208f3452b266ffd54fc2122d6f05a046
Osamushi <mad.naname@gmail.com>
2015-04-16 20:32:59 +0900

    Initial Commit

---
+++ SocketApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocketApp
+//
+//  Created by 加藤直人 on 4/16/15.
+//  Copyright (c) 2015 加藤直人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
